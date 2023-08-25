8de96b52e8d26fa81056c2986407c67297813802
ssbuild <462304@qq.cn>
2023-08-25 17:07:58 +0800

    xverse chat
    Signed-off-by: ssbuild <462304@qq.cn>

--- module_setup.py
+++ module_setup.py
@@ -3,7 +3,7 @@
 # @Time    : 2023/8/8 9:55
 from deep_training.utils.hf import register_transformer_model,register_transformer_config
 # from deep_training.nlp.models.xverse.modeling_xverse import XverseForCausalLM,XverseConfig
-from aigc_zoo.model_zoo.xverse.llm_model import XverseForCausalLM,XverseConfig
+from aigc_zoo.model_zoo.xverse.llm_model import MyXverseForCausalLM,XverseConfig
 from transformers import AutoModelForCausalLM
 
 __all__ = [
@@ -16,4 +16,4 @@ __all__ = [
 def module_setup():
     # 导入模型
     register_transformer_config(XverseConfig)
-    register_transformer_model(XverseForCausalLM, AutoModelForCausalLM)
+    register_transformer_model(MyXverseForCausalLM, AutoModelForCausalLM)
